6c69397ed9e736a4c875518bf6588c74a62abb47
SergeyBakunin <serge.bakunin@gmail.com>
2022-05-04 14:30:26 +0300

    Добавьте файлы проекта.

---
+++ HW_1/Exercise_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_1
+{
+    internal class Exercice_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            string userName = Console.ReadLine();
+            DateTime now = DateTime.Now;
+            Console.WriteLine($"Привет, {userName}, сегодня {now.ToString("D")}.");
+            Console.ReadLine();
+        }
+    }
+}
